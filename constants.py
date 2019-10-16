af5ed3a84763278e0662142f027e803ec238658c
Ben Lapuhapo <ben.lapuhapo@mths.ca>
2019-10-16 00:56:13 +0000

    Added space aliens game

---
+++ constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created by: Ben Lapuhapo
+# Created on: OCT 2019
+# This module contains constants for the alien game 
+
+# CircuitPython screen size is 160x120 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 120
+SCREEN_GRID_X = 16
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 60
